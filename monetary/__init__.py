7b7cf040fbc56f0c26542a7fbebf26dacfde1772
Brightcells <brightcells@gmail.com>
2018-03-22 03:43:15 +0800

    :tada: Initial commit.

---
+++ monetary/__init__.py
@@ -0,0 +1,12 @@
+from monetary.main import (
+    cent,
+    Fen,
+    dollar2cent,
+    Yuan2Fen,
+    dollar,
+    Yuan,
+    cent2dollar,
+    Fen2Yuan,
+    mul,
+    div,
+)
---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import monetary
+
+
+class TestMonetaryCommands(object):
+    def test_cent(self):
+        assert monetary.cent(0.07) == 7
+
+    def test_dollar(self):
+        assert monetary.dollar(7) == 0.07
+
+    def test_mul(self):
+        assert monetary.mul(1, 0.95) == 0.95
+        assert monetary.mul(1, 0.01) == 0.01
+
+    def test_div(self):
+        assert monetary.div(10, 2) == 5.0
+        assert monetary.div(10, 2.0) == 5.0
+        assert monetary.div(10, 2, cast_func=int) == 5
+        assert monetary.div(10, 2.0, cast_func=int) == 5
